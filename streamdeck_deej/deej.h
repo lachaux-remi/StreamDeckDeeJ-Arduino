1e98396c357920bc2ceec031e7f260e48c381a19
Rémi LACHAUX <contact@remi-lachaux.fr>
2022-06-03 12:52:02 +0200

    perf: remove debouncer

--- streamdeck_deej/deej.h
+++ streamdeck_deej/deej.h
@@ -1,5 +1,4 @@
 const int analogInputs[5] = { A10, A0, A1, A2, A3 };
-ulong lastDebounceTime = 0;
 
 void deej_setup() {
     for ( int i = 0; i < 5; i++ ) {
@@ -8,15 +7,11 @@ void deej_setup() {
 }
 
 void deej_loop() {
-    if ( ( millis() - lastDebounceTime ) > 500 ) {
-        lastDebounceTime = millis();
-
-        String input = "{\"type\":\"deej\",\"value\":{";
-        for ( int i = 0; i < 5; i++ ) {
-            input += "\"" + String( i ) + "\":" + String( analogRead( analogInputs[i] ) );
-            if ( i < 4 ) input += ",";
-        }
-        input += "}}";
-        Serial.println( input );
+    String input = "{\"type\":\"deej\",\"value\":{";
+    for ( int i = 0; i < 5; i++ ) {
+        input += "\"" + String( i ) + "\":" + String( analogRead( analogInputs[i] ) );
+        if ( i < 4 ) input += ",";
     }
+    input += "}}";
+    Serial.println( input );
 }
