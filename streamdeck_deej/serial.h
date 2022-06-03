756cef941883475b21a89cdb5055c5d432bb70b6
Rémi LACHAUX <contact@remi-lachaux.fr>
2022-06-03 12:27:02 +0200

    feat: add multiple key for serial command

--- streamdeck_deej/serial.h
+++ streamdeck_deej/serial.h
@@ -13,11 +13,11 @@ void serial_loop() {
         if ( command.startsWith("ir") ) { // ir:914,876,894,876,1794,862,894,876,894,876,894,1762,1810,848,894,1762,896,876,1780,878,894
             command.replace( "ir:", "" );
 
-            int numStart = 0;
-            int current_data_length = 0;
+            uint8_t numStart = 0;
+            uint8_t current_data_length = 0;
             uint16_t signal[30];
 
-            for ( int i = 0; i <= command.length(); ++i ) {
+            for ( uint8_t i = 0; i <= command.length(); ++i ) {
                 if ( command.charAt(i) == ',' || i == command.length() ) {
                     signal[current_data_length++] = command.substring( numStart, i ).toInt();
                     numStart = i + 1;
@@ -27,7 +27,16 @@ void serial_loop() {
             IrSender.sendRaw( signal, current_data_length, NEC_KHZ );
         } else if ( command.startsWith("key") ) { // key:248
             command.replace( "key:", "" );
-            Keyboard.write( command.toInt() );
+
+            uint8_t numStart = 0;
+            for ( uint8_t i = 0; i <= command.length(); ++i ) {
+                if ( command.charAt(i) == ',' || i == command.length() ) {
+                    Keyboard.press( command.substring( numStart, i ).toInt() );
+                    numStart = i + 1;
+                }
+            }
+
+            Keyboard.releaseAll();
         }
 
         command = "";
